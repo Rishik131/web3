81157bbb63906210d59e70095843614a1019b117
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-03 21:18:39 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,13 @@
 import { useState } from 'react';
-import "./dist/output.css";
-import Navbar from "./components/Navbar/Navbar";
-import Welcome from './components/Welcome/Welcome';
-import Transactions from './components/Transactions/Transactions';
-import Services from './components/Services/Services';
-import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar';
 
 const App = () => {
+
   return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar/>
-        <Welcome/>
-      </div>
-      <Services/>
-      <Transactions/>
-      <Footer/>
+    <div>
+      <Navbar/>
     </div>
   )
 }
 
-export default App;
+export default App
--- client/src/components/Welcome/Welcome.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
-}
-export default Welcome;
