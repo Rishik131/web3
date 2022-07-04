f87af0d306d22e3a1207baa789763cc9ba7c77aa
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-04 19:27:16 +0530

    welcome1

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import {SiEthereum} from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+import Loader from './Loader';
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className="flex w-full justify-center items-center">
+        <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+            hello
+        </div>
+    </div>
+//    <div>hello</div>
   )
 }
 
