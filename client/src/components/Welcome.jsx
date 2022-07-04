f1f739f80402fd6474f222fe880ba03f13ad3266
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-04 19:30:18 +0530

    welcome2

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,10 +8,13 @@ const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
         <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-            hello
+            <div className='flex flex-1 jusitfy-start flex-col md:mr-10'>
+                <h1 className='text-3xl sm:text-5xl tex-white text-gradient py-1'>
+                    Send Crypto <br /> across the world
+                </h1>
+            </div>
         </div>
     </div>
-//    <div>hello</div>
   )
 }
 
