63b42ae05cf14c561eaf2b6b7e4a5a6c52320768
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-05 13:08:31 +0530

    welcome3

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,13 +5,23 @@ import {BsInfoCircle} from 'react-icons/bs';
 import Loader from './Loader';
 
 const Welcome = () => {
-  return (
+  const connectWallet = () =>{
+
+  }
+  
+    return (
     <div className="flex w-full justify-center items-center">
         <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
             <div className='flex flex-1 jusitfy-start flex-col md:mr-10'>
                 <h1 className='text-3xl sm:text-5xl tex-white text-gradient py-1'>
                     Send Crypto <br /> across the world
                 </h1>
+                <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
+                    Explore the Crypto World. Buy and Sell crypto anywhere.
+                </p>
+                <button type='button' onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                    <p className='text-white text-base font-semibold'>Connect Wallet</p>
+                </button>
             </div>
         </div>
     </div>
