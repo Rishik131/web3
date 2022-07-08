9a8790029b221d7d06e94685599eea401100e6a3
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-08 19:56:56 +0530

    input

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,10 +6,21 @@ import Loader from './Loader';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
+// const Input = ({ placeholder, name, type, value, handleChange }) => (
+//     <input
+//         placeholder={placeholder}
+//         type={type}
+//         step="0.0001"
+//         value={value}
+//         onChange={(e) => handleChange(e, name)}
+//         className = "my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+//     />
+// )
+
 const Welcome = () => {
   const connectWallet = () =>{
 
-  }
+  };
   
     return (
     <div className="flex w-full justify-center items-center">
@@ -54,22 +65,25 @@ const Welcome = () => {
                                 <SiEthereum fontSize={21} color="#fff"/>
                             </div>
                             <BsInfoCircle fontSize={17} color="#fff"/>
-                            <div>
-                                <p className='text-white font-light text text-sm'>
-                                    #Adress
-                                </p>
-                                <p className='text-white font-semibold text text-lg mt-1'>
-                                    Ethereum
-                                </p>
-                            </div>
+                        </div>
+                        <div>
+                            <p className='text-white font-light text text-sm'>
+                                #Address
+                            </p>
+                            <p className='text-white font-semibold text text-lg mt-1'>
+                                Ethereum
+                            </p>
                         </div>
                     </div>
                 </div>
+                <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                    <input/>
+                    {/* <Input placeholder="Address" name="addressTo" type="text" handleChange={() => ()}/> */}
+                </div>
             </div>
-
         </div>
     </div>
-  )
+  );
 }
 
 export default Welcome
