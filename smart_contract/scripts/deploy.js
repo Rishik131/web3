123c1e0030ee8234e68ea1b7d055e1809f7cbd68
Rishik Kumar <rishik.k2002@gmail.com>
2022-07-12 12:43:13 +0530

    smart contract3

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -2,18 +2,10 @@
 const hre = require("hardhat");
 
 const main = async () => {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+  await transactions.deployed();
+  console.log("Transactions deployed to: ", transactions.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -21,7 +13,11 @@ const main = async () => {
 const runMain = async () => {
   try{
       await main();
+      process.exit(0);
   } catch(error){
-
+    console.error(error);
+    process.exit(1);
   }
 }
+
+runMain();
