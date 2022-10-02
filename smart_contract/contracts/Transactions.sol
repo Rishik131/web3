34ce043449271aeacead49ba9cd9018c3243e97b
Rishik Kumar <rishik.k2002@gmail.com>
2022-10-02 21:51:20 +0530

    functions

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,6 +2,30 @@
 pragma solidity ^0.8.0;
 contract Transactions
 {
-    uint256 TransactionCounter;
+    uint256 TransactionCount;
     event transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public
+    {
+        TransactionCount += 1;
+        transactions.push(TransferStruct(msg.sender,receiver,amount, message,block.timestamp,keyword));
+        emit Transfer(msg.sender,receiver,amount, message,block.timestamp,keyword)
+    }
+    function getAllTransactions() public view returns (TransferStruct[] memory)
+    {
+
+    }
+    function getTransactionCount() public view returns(uint256)
+    {
+
+    }
 }
