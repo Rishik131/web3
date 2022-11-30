d72b9e0666cd7ca4782e6493ac669daf44e67d07
Rishik Kumar <rishik.k2002@gmail.com>
2022-11-30 16:04:24 +0530

    deploy1

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,1 +1,41 @@
-// 0x2dBaBd479e981343f7f84aEEDe1725CeE2b67910
+import React, { useState, useEffect } from "react";
+import { ethers } from 'ethers';
+import { contractABI, contractAddress } from '../utils/constants';
+export const TransactionContext = React.createContext();
+const { ethereum } = window;
+const getEthereumContract = () => {
+    const provider = new ethers.providers.webProvider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
+    console.log({
+        provider,
+        signer,
+        transactionContract
+    });
+}
+export const TransactionProvider = ({ children }) => {
+    const checkIfWalletIsConnected = async () => {
+        if (!ethereum) return alert("Please install metamask");
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        console.log(accounts);
+    }
+    const connectWallet = async () => {
+        try {
+            if (!ethereum) return alert("Please install metamask");
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            setCurrentAccount(accounts[0]);
+        }
+        catch (error) {
+
+        }
+
+    }
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
+    return (
+        <TransactionContext.Provider value={{ value: 'test' }}>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
