99a4d576088748a3f038537a4576a2df1158ac6c
Rishik Kumar <rishik.k2002@gmail.com>
2022-12-18 11:33:40 +0530

    contract3

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -13,6 +13,8 @@ const getEthereumContract = () => {
 export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState('');
     const [formData, setFormData] = useState({addressTo: '', amount:'', keyword:'', message:''});
+    const [isLoading,setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
     const handleChange = (e,name) => {
         setFormData((prevState) => ({...prevState,[name]:e.target.value})); 
     }
@@ -60,7 +62,15 @@ export const TransactionProvider = ({ children }) => {
                     gas: '0x5208',
                     value: parsedAmount._hex,
                 }]
-            })
+            });
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message,keyword);
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+            transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber());
         } catch (error) {
             console.log(error);
             throw new Error("No ethereum object.");
